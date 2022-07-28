c530a6a256b61aae873a72267c4661010d0f4436
ngdinhhieu <108282201+ngdinhhieu@users.noreply.github.com>
2022-07-28 17:04:52 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,11 +4,12 @@ point = prompt('Nhập số điểm cần để chiến thắng',0);
 const cells = Array.from(document.querySelectorAll('.cell'));
 const resetButton = document.querySelector('.reset');
 const announcer = document.querySelector('.announcer');
-const scoreX = document.querySelector('.turnsX');
-const scoreO = document.querySelector('.turnsO');
-const gradeX = document.querySelector('#score_X');
-const gradeO = document.querySelector('#score_O');
+const goalX = document.querySelector('.goal_X');
+const goalO = document.querySelector('.goal_O');
+const scoreX = document.querySelector('#score_X');
+const scoreO = document.querySelector('#score_O');
 const restartButton = document.querySelector('.restart');
+const clap = document.querySelector('.clap');
 
 let board = ['','','','','','','','',''];
 let currentPlayer = 'X';
@@ -18,31 +19,29 @@ const PLAYERX_WON = 'PLAYERX_WON';
 const PLAYERO_WON = 'PLAYERO_WON';
 const TIE = 'TIE';
 
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-function handleResultValidation() {
+function checkWinCondition() {
     let roundWon = false;
-    for (let i = 0; i <= 7; i++) {
-        const winCondition = winningConditions[i];
-        const a = board[winCondition[0]];
-        const b = board[winCondition[1]];
-        const c = board[winCondition[2]];
-        if (a === '' || b === '' || c === '') {
-            continue;
-        }
-        if (a === b && b === c) {
-            roundWon = true;
-            break;
-        }
+    if ((board[0]==='X' && board[1]==='X' && board[2]==='X')||
+    (board[3]==='X' && board[4]==='X' && board[5]==='X')||
+    (board[6]==='X' && board[7]==='X' && board[8]==='X')||
+    (board[0]==='X' && board[3]==='X' && board[6]==='X')||
+    (board[1]==='X' && board[4]==='X' && board[7]==='X')||
+    (board[2]==='X' && board[5]==='X' && board[8]==='X')||
+    (board[0]==='X' && board[4]==='X' && board[8]==='X')||
+    (board[2]==='X' && board[4]==='X' && board[6]==='X'))
+    {
+        roundWon = true;
+    }
+    else if ((board[0]==='O' && board[1]==='O' && board[2]==='O')||
+    (board[3]==='O' && board[4]==='O' && board[5]==='O')||
+    (board[6]==='O' && board[7]==='O' && board[8]==='O')||
+    (board[0]==='O' && board[3]==='O' && board[6]==='O')||
+    (board[1]==='O' && board[4]==='O' && board[7]==='O')||
+    (board[2]==='O' && board[5]==='O' && board[8]==='O')||
+    (board[0]==='O' && board[4]==='O' && board[8]==='O')||
+    (board[2]==='O' && board[4]==='O' && board[6]==='O'))
+    {
+        roundWon = true;
     }
 
 if (roundWon) {
@@ -58,27 +57,29 @@ if (!board.includes(''))
 const announce = (type) => {
     switch(type){
         case PLAYERO_WON:
-            scoreO.textContent=Number(scoreO.textContent)+1;
+            goalO.textContent=Number(goalO.textContent)+1;
             alert("O won!");
             resetButton.classList.remove('hide');
-            if(scoreO.textContent === point){
-                gradeO.innerText=Number(gradeO.innerText)+1;
+            if(goalO.textContent === point){
+                scoreO.innerText=Number(scoreO.innerText)+1;
                 announcer.innerHTML = 'Player <span class="playerO">O</span> won!!!';
                 announcer.classList.remove('hide');
                 restartButton.classList.remove('hide');
-                resetButton.classList.add('hide')
+                resetButton.classList.add('hide');
+                clap.classList.remove('hide');
             };
             break;  
         case PLAYERX_WON:
-            scoreX.textContent=Number(scoreX.textContent)+1;
+            goalX.textContent=Number(goalX.textContent)+1;
             alert("X won!");
             resetButton.classList.remove('hide')
-            if (scoreX.textContent === point){
-                gradeX.innerText=Number(gradeX.innerText)+1;
+            if (goalX.textContent === point){
+                scoreX.innerText=Number(scoreX.innerText)+1;
                 announcer.innerHTML = 'Player <span class="playerX">X</span> won!!!';
                 announcer.classList.remove('hide');
                 restartButton.classList.remove('hide');
                 resetButton.classList.add('hide')
+                clap.classList.remove('hide');
             };
             break;
         case TIE:
@@ -99,7 +100,12 @@ const updateBoard = (index) => {
 }
 
 const changePlayer = () => {
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    if (currentPlayer === 'X'){
+        currentPlayer = 'O';
+    }
+    else if (currentPlayer === 'O'){
+        currentPlayer = 'X';
+    }
 }
 
 const userAction = (cell, index) => {
@@ -107,7 +113,7 @@ const userAction = (cell, index) => {
         cell.innerText = currentPlayer;
         cell.classList.add('player' + currentPlayer);
         updateBoard(index);
-        handleResultValidation();
+        checkWinCondition();
         changePlayer();
     }
 }
@@ -129,13 +135,12 @@ const resetBoard = () => {
     });
 }
 
-const restartBoard = () => {
+const reloadGame = () => {
     resetBoard();
-    gradeX.innerText='0';
-    gradeO.innerText='0';
-    scoreO.textContent='0';
-    scoreX.textContent='0';
+    goalO.textContent='0';
+    goalX.textContent='0';
     restartButton.classList.add('hide');
+    clap.classList.add('hide');
     point = prompt('Nhập số điểm cần để chiến thắng',0);
 }
 
@@ -144,4 +149,4 @@ cells.forEach( (cell, index) => {
 });
 
 resetButton.addEventListener('click',resetBoard);
-restartButton.addEventListener('click',restartBoard);
+restartButton.addEventListener('click',reloadGame);
